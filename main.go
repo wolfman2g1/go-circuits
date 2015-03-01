456a6b0b42efca12f4f33401a32297bc2637ddb0
Ryan Jones <ryan.jones26@gmail.com>
2015-02-28 22:03:49 -0500

    added post request handler

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
+
 import (
-  "fmt"
-  "net/http"
-  )
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-  func handler( w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello"))
-  }
+type PostStruct struct {
+	name    string
+	message string
+}
 
-  func main() {
-    fmt.Println("Starting server on port 3000")
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":3000", nil)
-  }
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+}
+
+func main() {
+	fmt.Println("Starting server on port 3000")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/post", postHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	input := json.NewDecoder(r.Body)
+	var p PostStruct
+	err := input.Decode(&p)
+	if err != nil {
+		panic(err)
+	}
+
+}
