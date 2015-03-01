f3efe76eeb467d957b4fc9b5092ef4c5f47e11c1
itsmontoya <josh@meteora.co>
2015-03-01 11:37:28 -0800

    Small fixes so it actually compiles

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"log"
-	
+	"net/http"
+
 	"github.com/boltdb/bolt"
 )
 
 func main() {
-	db,err := bolt.Open("test.db", 0600, nil)
+	db, err := bolt.Open("test.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	s := server{
-		db : db,
+		db: db,
 	}
-	
+
 	s.initialize()
-	
+
 	fmt.Println("Starting server on port 3000")
 	http.HandleFunc("/ping", s.getPing)
 	http.HandleFunc("/change", s.postChange)
@@ -31,71 +31,74 @@ type server struct {
 	db *bolt.DB
 }
 
-// This is for a ping request. 
-func (s *server) getPing(w http.ResponseWriter, r *http.Request){
+// This is for a ping request.
+func (s *server) getPing(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
 }
 
 // This is to handle the 'change' route. Will determine if POST or GET
-func (s *server) change(w http.ResponseWriter, r *http.Request){
-	if(r.Method == "POST"){
+func (s *server) change(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
 		s.postChange(w, r)
-	} else if (r.Method == "GET"){
+	} else if r.Method == "GET" {
 		s.getChange(w, r)
 	}
 }
 
 // This is for getting a change request
-func (s *server) getChange(w http.ResponseWriter, r *http.Request){
+func (s *server) getChange(w http.ResponseWriter, r *http.Request) {
 	s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(`changes`))
-		
+
 		b.ForEach(func(key, value []byte) error {
-			log.Println(string(key), string(value))			
+			log.Println(string(key), string(value))
+
+			return nil
 		})
+
+		return nil
 	})
-	fmt.Println(c)	
 }
 
 // This is for posting a change request
-func (s *server) postChange(w http.ResponseWriter, r *http.Request){
+func (s *server) postChange(w http.ResponseWriter, r *http.Request) {
 	var c change
-	
+
 	err := json.NewDecoder(r.Body).Decode(&c)
 	if err != nil {
 		// We are doing a console log, rather than panicing. This usually means the
 		// inbound json was invalid
 		log.Println("Error parsing JSON:", err)
 	}
-	
+
 	s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(`changes`))
-		
+
 		// When you put a key/value into bolt, it needs to be
 		// in the form of a byteslice ([]byte).
-		// Usually here, you would marshal your struct into 
-		// json, but we are going to keep it simple for now. 
+		// Usually here, you would marshal your struct into
+		// json, but we are going to keep it simple for now.
 		b.Put([]byte(c.Name), []byte(c.Message))
 		return nil
 	})
-	fmt.Println(c)	
+	fmt.Println(c)
 }
 
 // Do anything you need to setup before the server actually runs
-func (s *server) initialize(){
+func (s *server) initialize() {
 	s.db.Update(func(tx *bolt.Tx) error {
 		// Make sure you create the buckets you need,
 		// if you try to access a bucket which doesn't exist
 		// you will get a panic
 		tx.CreateBucket([]byte(`changes`))
 		return nil
-	})	
+	})
 }
 
 // When you progress, this will be called when you catch a SIGINT.
 // For now, it will just sit here
-func (s *server) close(){
-	s.db.Close()	
+func (s *server) close() {
+	s.db.Close()
 }
 
 // Generally, I try to keep my structs private unless I know I will
