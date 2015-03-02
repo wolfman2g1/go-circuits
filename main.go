c75e18cec84a3523a955c8e61540ebe8783db98d
Ryan Jones <ryan.jones26@gmail.com>
2015-03-01 23:59:32 -0500

    lost

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+"fmt"
+"log"
+"github.com/gin-gonic/gin"
+"encoding/json"
+"github.com/boltdb/bolt"
+)
+
+
+type carrierJSON struct {
+   CarrierName string 'json: "carrier_name"  binding: "required"'
+   SupportEmail string 'json: "support_email"  binding: "required"'
+   SupportNum string 'json: "support_num"  binding: "required"'
+}
+
+type circuitJSON struct {
+  CircuitId string 'json: "circuit_id"  binding: "required"'
+  CircuitLoc string 'json: "circuit_loc"  binding: "required"'
+  CarrierBlock string 'json: "circuit_block"  binding: "required"'
+
+}
 
 func main() {
+  // opden database
+  db,err := bolt.Open("go-circuits.db", 0600, nil)
+  if err != nil {
+    log.Fatal(err)
+
+  }
 	router := gin.Default()
 	// test to make sure the server is listening
 	router.GET("/ping", func(c *gin.Context) {
@@ -28,3 +54,4 @@ func main() {
 	// listen on port 3000
 	r.Run(":3000")
 }
+ func getCarriers(w h)
