513e9bc2f4598ec760968d66541faa7a4227dcdc
Ryan Jones <ryan.jones26@gmail.com>
2015-03-02 02:41:22 -0500

    ported to gin

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-"fmt"
-"log"
-"github.com/gin-gonic/gin"
-"encoding/json"
-"github.com/boltdb/bolt"
+	"encoding/json"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/gin-gonic/gin"
+	"log"
 )
-
-
 type carrierJSON struct {
    CarrierName string 'json: "carrier_name"  binding: "required"'
    SupportEmail string 'json: "support_email"  binding: "required"'
@@ -21,37 +19,61 @@ type circuitJSON struct {
   CarrierBlock string 'json: "circuit_block"  binding: "required"'
 
 }
+type server struct {
+  db *bolt.DB
+}
 
 func main() {
-  // opden database
-  db,err := bolt.Open("go-circuits.db", 0600, nil)
+  db,err := bolt.Open("test.db", 0600, nil)
   if err != nil {
     log.Fatal(err)
-
   }
-	router := gin.Default()
-	// test to make sure the server is listening
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	carrier := router.Group("/carriers")
-
-	{
-		carrier.GET("/list", getCarriers)
-		carrier.POST("create", createCarriers)
-		carrier.DELETE("/delete", deleteCarriers)
-		carrier.PUT("/update", updateCarriers)
-
-	}
-
-	circuit := router.Group("/circuits")
-	{
-		carrier.GET("/list", getCircuits)
-		carrier.POST("/create", createCircuits)
-		carrier.DELETE("/delete", deleteCircuits)
-		carrier.POST("/update", updateCircuits)
-	}
-	// listen on port 3000
-	r.Run(":3000")
+  
+  s := server{
+    db : db,
+  }
+  
+  s.initialize()
+  router := gin.Default()
+  router.GET("/ping",func(c *gin.Context){
+    c.String(200, "pong")
+    })
+  router.GET("/circuits", s.getCircuits)
 }
- func getCarriers(w h)
+
+func (s *server) getCircuits(c *gin.Context) {
+  s.db.Update(func(tx *bolt.Tx) error {
+    b:= tx.Bucket([]byte('circuits'))
+    b.ForEach(func(key, value []byte) error {
+      log.Println(string(key), string(value))
+      
+    })
+    
+  })
+}
+
+func (s *server) postCircuits(c *gin.Context){
+  var cirs circuitJSON
+  err :=  c.Bind(&cirs)
+  if err != nil {
+    // inbound json was invalid
+    log.Println("Error parsing JSON:", err)
+  }
+    s.db.Update(func(tx *bolt.Tx) error {
+      b:= tx.Bucket([]byte('circuits'))
+      b.Put([]byte(cirs.CircuitID), []byte(cirs.CircuitLoc), []byte(cirs.CarrierBlock))
+      return nil
+      
+    })
+    fmt.Println(cirs)
+}
+// Do anything you need to setup before the server actually runs
+func (s *server) initialize(){
+  s.db.Update(func(tx *bolt.Tx) error {
+    // Make sure you create the buckets you need,
+    // if you try to access a bucket which doesn't exist
+    // you will get a panic
+    tx.CreateBucket([]byte(`circuits`))
+    return nil
+  })  
+}
