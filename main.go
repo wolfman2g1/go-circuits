49e16c0d63755d7508e9307e2ca0ff49a4223134
Ryan Jones <ryan.jones26@gmail.com>
2015-03-01 22:26:08 -0500

    switched to gin framework
    started writing the basic routes for my api

--- main.go
+++ main.go
@@ -1,116 +1,30 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/boltdb/bolt"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	db, err := bolt.Open("test.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s := server{
-		db: db,
-	}
-	defer s.close()
-
-	s.initialize()
-
-	fmt.Println("Starting server on port 3000")
-	http.HandleFunc("/ping", s.getPing)
-	http.HandleFunc("/change", s.postChange)
-	http.ListenAndServe(":3000", nil)
-}
-
-type server struct {
-	db *bolt.DB
-}
-
-// This is for a ping request.
-func (s *server) getPing(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong"))
-}
-
-// This is to handle the 'change' route. Will determine if POST or GET
-func (s *server) change(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		s.postChange(w, r)
-	} else if r.Method == "GET" {
-		s.getChange(w, r)
-	}
-}
-
-// This is for getting a change request
-func (s *server) getChange(w http.ResponseWriter, r *http.Request) {
-	s.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(`changes`))
-
-		b.ForEach(func(key, value []byte) error {
-			log.Println(string(key), string(value))
-
-			return nil
-		})
-
-		return nil
+	router := gin.Default()
+	// test to make sure the server is listening
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
 	})
-}
+	carrier := router.Group("/carriers")
 
-// This is for posting a change request
-func (s *server) postChange(w http.ResponseWriter, r *http.Request) {
-	var c change
+	{
+		carrier.GET("/list", getCarriers)
+		carrier.POST("create", createCarriers)
+		carrier.DELETE("/delete", deleteCarriers)
+		carrier.PUT("/update", updateCarriers)
 
-	err := json.NewDecoder(r.Body).Decode(&c)
-	if err != nil {
-		// We are doing a console log, rather than panicing. This usually means the
-		// inbound json was invalid
-		log.Println("Error parsing JSON:", err)
-		return
 	}
-	//check to make sure the change struct is not nil
-	if c.Name == "" || c.Message == "" {
-		w.Write([]byte("Invalid input"))
-		return
+
+	circuit := router.Group("/circuits")
+	{
+		carrier.GET("/list", getCircuits)
+		carrier.POST("/create", createCircuits)
+		carrier.DELETE("/delete", deleteCircuits)
+		carrier.POST("/update", updateCircuits)
 	}
-	s.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(`changes`))
-
-		// When you put a key/value into bolt, it needs to be
-		// in the form of a byteslice ([]byte).
-		// Usually here, you would marshal your struct into
-		// json, but we are going to keep it simple for now.
-		b.Put([]byte(c.Name), []byte(c.Message))
-		w.Write([]byte("Saved!"))
-		return nil
-	})
-	fmt.Println(c)
-}
-
-// Do anything you need to setup before the server actually runs
-func (s *server) initialize() {
-	s.db.Update(func(tx *bolt.Tx) error {
-		// Make sure you create the buckets you need,
-		// if you try to access a bucket which doesn't exist
-		// you will get a panic
-		tx.CreateBucket([]byte(`changes`))
-		return nil
-	})
-}
-
-// When you progress, this will be called when you catch a SIGINT.
-// For now, it will just sit here
-func (s *server) close() {
-	s.db.Close()
-}
-
-// Generally, I try to keep my structs private unless I know I will
-// be exporting them.
-type change struct {
-	Name    string `json:"name"`
-	Message string `json:"message"`
+	// listen on port 3000
+	r.Run(":3000")
 }
