e98aff545c9a185218f650d9af1e62c7ef8e3dc1
Ryan Jones <ryan.jones26@gmail.com>
2015-05-05 21:54:28 -0400

    fixed some typos

--- datastore.go
+++ datastore.go
@@ -5,27 +5,27 @@ import (
   "github.com/jinzhu/gorm"
   )
 type Carrier struct {
-  ID int 'sql:"AUTO_INCREMENT"'
-  CarrierName string  'sql:"size:255"'
+  ID int `sql:"AUTO_INCREMENT"`
+  CarrierName string  `sql:"size:255"`
   SupportEmail string
   SupportNum string
   CarrierStreet string
   CarrierCity string
   CarrierState string
   CarrierPostal string
-  CreatedAt time.time
-  UpdatedAt time.time
+  CreatedAt time.Time
+  UpdatedAt time.Time
   Circuits []Circuit // has many circuits
 
 }
 type Circuit struct {
-  ID int 'sql:"AUTO_INCREMENT"'
-  CircuitID string 'sql:"size:255"'
+  ID int `sql:"AUTO_INCREMENT"`
+  CircuitID string `sql:"size:255"`
   ALoc string
   ZLoc string
-  CarrierBlock string 'sql:"size:255"'
-  ExternalBlock string 'sql:"size:255"'
+  CarrierBlock string `sql:"size:255"`
+  ExternalBlock string `sql:"size:255"`
   CircuitID // belongs to Carrier
-  CreatedAt time.time
-  UpdatedAt time.time
+  CreatedAt time.Time
+  UpdatedAt time.Time
 }
